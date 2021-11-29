1ad11d1ce4e9dcb9b5f507e1e51c3fa8298d20dd
evulliam <evulliam@sfu.ca>
2021-11-29 15:16:48 -0800

    Split the process_and_merge() function into smaller component functions. Added functionality to take the filepaths on the terminal

--- Model.py
+++ Model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import re
 import nltk
 import datetime 
+import sys
 from nltk.sentiment import SentimentIntensityAnalyzer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
@@ -14,6 +15,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestRegressor
 
 # This function is for Sentiment analysis 
+# NOTE: This code is now handled by the sparked.py
+# temporarily keeping for reference 
 def sent(text):
     sia = SentimentIntensityAnalyzer()
     res = sia.polarity_scores(str(text))
@@ -26,6 +29,8 @@ def sent(text):
     else:
         return res['pos']
 
+# NOTE: This code is now handled by the sparked.py 
+# temporarily keeping for reference
 def sentiment_analysis():
     # This code doesn't need to be run, but i will include it so you can see how 
     # I got the 'reddit_results.csv' file
@@ -46,7 +51,51 @@ def sentiment_analysis():
     # It takes a WHILE to run, so I will just include the csv file of the results
     # reddit_data['sentiment'] = reddit_data['body'].apply(sent)
     # reddit_data.to_csv('reddit_results.csv', index=False)
+    
+# Input: Takes in dataframe of 'coin_Bitcoin.csv' or other related coin datasets
+# Output: A dataframe with added labels for increasing/not increasing
+def create_labels(df):
+    df['NextClose'] = df['Close'].shift(-1)
+    df.loc[df['NextClose'] > df['Close'], 'Change'] = 'Increase'
+    df.loc[df['NextClose'] <= df['Close'], 'Change'] = 'No Increase'
+    df = df.dropna()
+    
+    vals = df.groupby('Change').count()
+    ratio = vals.loc['Increase']['SNo'] / (vals.loc['Increase']['SNo'] + vals.loc['No Increase']['SNo'])
+    
+    # Show the ratio of Increase to No Increase rows. This will indicate if the dataset is inbalanced 
+    # if it far from a 50/50 split
+    print(ratio, 1-ratio)
+    
+    return df
 
+# Input: Takes in dataframe with column 'date
+# Output: Turns the date column into datetime object 
+def process_date(df):
+    df['date' ]= pd.to_datetime(df['date'], errors='coerce').dt.date
+    return df
+
+# Input: Dataframe from the currency (eg. coin_Bitcoin.csv) datasets
+# Output: removes unused columns and sets index to date
+def process_currency(df):
+    bit_info = df.drop(['SNo', 'Name', 'Symbol'], axis=1)
+    bit_info = bit_info.rename(columns={'Date': 'date'})
+    # df['date_column'] = pd.to_datetime(df['datetime_column']).dt.date
+    bit_info['date'] = pd.to_datetime(bit_info['date']).dt.date
+    # bit_info = bit_info.groupby('date').sum()
+    
+    return bit_info
+    
+
+# Input: Two dataframes with 'date' column
+# Output: The result of merging the two dataframes
+def merge_on_date(df1, df2):
+    merging = df1.merge(df2, on='date')
+    return merging
+
+       
+# Most of the processing is now already taken care of by the sparked.py
+# temporarily keeping for reference
 def process_and_merge():
     reddit_data = pd.read_csv('reddit_results.csv', dtype='object')
     group_data = reddit_data.drop(['created_utc', 'body'], axis=1)
@@ -96,7 +145,7 @@ def run_model(df):
         )
 
     model.fit(X_train, y_train)
-    # ONLY INCLUDE SCORE IN THE FINAL SUBMISSION
+
     print(model.score(X_valid, y_valid))
     print(model.score(X_train, y_train))
 
@@ -110,9 +159,36 @@ def run_model(df):
     
 def main():
     
+    # To run:
+    # python3 Model.py coin_Bitcoin.csv DailyAverages.csv
+    # OR
+    # python3 Model.py coin_Bitcoin.csv DailySum.csv
+    
+    # for now we are just merging two datasets
+    in_dir1 = sys.argv[1] # coin_Bitcoin.csv (or other currency datasets)
+    in_dir2 = sys.argv[2] # one of (DailyAverages.csv, DailySum.csv)
+    
+    df_bitcoin = pd.read_csv(in_dir1, parse_dates=['Date'])
+    df_reddit = pd.read_csv(in_dir2, parse_dates=['date'])
+    
+    # Pre processing for coin_Bitcoin.csv (or other currency datasets)
+    df_bitcoin = create_labels(df_bitcoin)
+    df_bitcoin = process_currency(df_bitcoin)
+    df_bitcoin = process_date(df_bitcoin)
+    
+    # Pre processing for one of (DailyAverages.csv, DailySum.csv)
+    df_reddit = process_date(df_reddit)
+    
+    print(df_reddit)
+    print(df_bitcoin)
+    
+    merged = merge_on_date(df_bitcoin, df_reddit)
+    
+    print(merged)
+    
     # sentiment_analysis()
-    df = process_and_merge()
-    run_model(df)
+    #df = process_and_merge()
+    # run_model(df)
     
     
 if __name__== '__main__':
